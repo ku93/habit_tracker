5a38012f52c13e4545f049009ee9c386c3e2aa8b
ku93 <tkachev1993adg@yandex.ru>
2025-04-23 20:33:40 +0300

    serializers

---
+++ habit/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Habit
+from .validators import (
+    validate_related_habit, validate_reward_or_related,
+    validate_pleasant_habit, validate_periodicity
+)
+
+class HabitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habit
+        fields = '__all__'
+        read_only_fields = ('user', 'created_at')
+
+    def validate(self, data):
+        habit = Habit(**data)
+        validate_related_habit(habit)
+        validate_reward_or_related(habit)
+        validate_pleasant_habit(habit)
+        validate_periodicity(habit.periodicity)
+        return data
+
+class PublicHabitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habit
+        fields = (
+            'id', 'place', 'time', 'action',
+            'periodicity', 'duration', 'is_public'
+        )
